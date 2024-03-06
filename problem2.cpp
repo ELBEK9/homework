220996871ca1eb18c665f65c90fecb817dde89fc
ELBEK9 <70363038+ELBEK9@users.noreply.github.com>
2024-03-06 20:18:48 +0500

    Update problem2.cpp

--- problem2.cpp
+++ problem2.cpp
@@ -14,21 +14,21 @@ public:
     this->seconds = s;
 }
     void changeTime(){
-        if (seconds >= 60) {
-            minutes += seconds / 60;
-            seconds %= 60;
+        if (seconds>=60) {
+            minutes= minutes + (seconds/60);
+            seconds= seconds%60;
         }
-        if (minutes >= 60) {
-            hours += minutes / 60;
-            minutes %= 60;
+        if (minutes>=60) {
+            hours= hours +(minutes/60);
+            minutes= minutes % 60;
         }
         if (hours >= 24) {
-            hours %= 24;
+            hours= hours % 24;
         }  
     }
     
     void printTime() {
-        cout<<hours<<" : "<<minutes<<" : "<<seconds<<endl;
+        cout<<hours<<":"<<minutes<<":"<<seconds<<endl;
     }
 
 };
