e41f1dbf645627d3ed0cbe66213d46452d8db792
ELBEK9 <70363038+ELBEK9@users.noreply.github.com>
2024-03-06 20:13:08 +0500

    Create problem3.cpp

---
+++ problem3.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <map>
+using namespace std;
+
+class Student{
+private:
+    string name;
+    long int id;
+    string groupName;
+    
+public:
+    Student(string n, int id, string g){
+        this->name = n; 
+        this->id = id; 
+        this->groupName = g;
+    }
+    
+    string& getGroupName() {
+        return groupName;
+    }
+};
+
+int main()
+{
+    int num_of_stud;
+    cin>>num_of_stud;
+    
+    vector<Student> students;
+    string name, groupName;
+    long int id;
+    
+    for (int i = 0;  i < num_of_stud; ++i) {
+        cin>>name>>id>>groupName;
+        students.emplace_back(name, id, groupName);
+    }
+    
+    map<string, int> groupCount;
+    for(auto it=students.rbegin();it!=students.rend();it++){
+        string& groupName = it->getGroupName();
+        groupCount[groupName]++;
+    }
+    
+    for (auto it = groupCount.begin(); it != groupCount.end(); it++) {
+        cout<<it->first<<": "<<it->second<<endl;
+    }
+    
+    return 0;
+}
