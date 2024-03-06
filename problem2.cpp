d395ad1a079ea20de808d9bff9c9bb46d44bd94e
ELBEK9 <70363038+ELBEK9@users.noreply.github.com>
2024-03-06 20:12:16 +0500

    Create problem2.cpp

---
+++ problem2.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+
+class Timer{
+private:
+    int hours;
+    int minutes;
+    int seconds;
+public:
+    Timer(int h, int m, int s) {
+    this->hours = h; 
+    this->minutes = m; 
+    this->seconds = s;
+}
+    void changeTime(){
+        if (seconds >= 60) {
+            minutes += seconds / 60;
+            seconds %= 60;
+        }
+        if (minutes >= 60) {
+            hours += minutes / 60;
+            minutes %= 60;
+        }
+        if (hours >= 24) {
+            hours %= 24;
+        }  
+    }
+    
+    void printTime() {
+        cout<<hours<<" : "<<minutes<<" : "<<seconds<<endl;
+    }
+
+};
+
+int main()
+{
+    int h, m, s;
+    cin>>h>>m>>s;
+    Timer timer(h, m, s);
+    timer.changeTime();
+    timer.printTime();
+
+    return 0;
+}
